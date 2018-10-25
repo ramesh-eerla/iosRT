daa7afa01889808932fdaca93407659b1fc351fb
S0tallytober <kalivarapuvenkatesh@gmail.com>
2018-10-25 23:30:23 +0530

    Splash Screen, Login & Register screens UI completed

---
+++ RoomTrac/RoomTrac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoomTrac
+//
+//  Created by Sunil on 25/10/18.
+//  Copyright © 2018 Sunil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
