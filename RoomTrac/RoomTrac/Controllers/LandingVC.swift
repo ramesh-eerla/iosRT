c80806c7983524cf098160a1404e19b07e0d43a4
sunilreddy1257 <sunilreddy1257@gmail.com>
2018-10-25 23:51:14 +0530

    added landing VC

---
+++ RoomTrac/RoomTrac/Controllers/LandingVC.swift
@@ -0,0 +1,30 @@
+//
+//  LandingVC.swift
+//  RoomTrac
+//
+//  Created by Sunil on 25/10/18.
+//  Copyright © 2018 Sunil. All rights reserved.
+//
+
+import UIKit
+
+class LandingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
